e4f3555588d1d6a3443c5db045c52bff6c4ceb5a
Josdelsan <joseM_DS@hotmail.com>
2022-01-14 16:40:13 +0100

    Entrega

--- src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomType.java
+++ src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomType.java
@@ -1,11 +1,29 @@
 package org.springframework.samples.petclinic.recoveryroom;
 
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.Size;
+
+import org.springframework.samples.petclinic.model.BaseEntity;
+import org.springframework.samples.petclinic.pet.Visit;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class RecoveryRoomType {
-    Integer id;
-    String name;
+@Entity
+public class RecoveryRoomType extends BaseEntity{
+
+    @Size(min = 3, max = 50)
+	@Column(unique = true)
+	private String name;
+
+    @OneToMany(mappedBy = "roomType")
+    private List<RecoveryRoom> recoveryRooms;
 }
--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -57,8 +57,17 @@ INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (11, 'Freddy', '201
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (12, 'Lucky', '2010-06-24', 2, 10);
 INSERT INTO pets(id,name,birth_date,type_id,owner_id) VALUES (13, 'Sly', '2012-06-08', 1, 10);
 
+insert into recovery_room_type(id,name) values (1,'room');
+insert into recovery_room_type(id,name) values (2,'box');
+
+
+
+insert into recovery_room(id,name,size,secure,recoveryroomtype_id) values (1,'Big room for dangerous animals',6.5,TRUE,1);
+insert into recovery_room(id,name,size,secure,recoveryroomtype_id) values (2,'Medium box',1.5,FALSE,2);
+
+
 INSERT INTO visits(id,pet_id,visit_date,description) VALUES (1, 7, '2013-01-01', 'rabies shot');
 INSERT INTO visits(id,pet_id,visit_date,description) VALUES (2, 8, '2013-01-02', 'rabies shot');
-INSERT INTO visits(id,pet_id,visit_date,description) VALUES (3, 8, '2013-01-03', 'neutered');
+INSERT INTO visits(id,pet_id,visit_date,description,recoveryroom_id) VALUES (3, 8, '2013-01-03', 'neutered',2);
 INSERT INTO visits(id,pet_id,visit_date,description) VALUES (4, 7, '2013-01-04', 'spayed');
 
